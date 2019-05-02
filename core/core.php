1d774a3fa7dcc20bf3b06ba323851bea24ecc104
Prof Langues <Prof_LVE@EDU.UA>
2019-05-02 13:58:54 +0200

    [9.1.00] version

--- core/core.php
+++ core/core.php
@@ -28,7 +28,7 @@ class common {
 	const GROUP_ADMIN = 3;
 
 	// Numéro de version 
-	const ZWII_VERSION = '9.1.00.dev63';
+	const ZWII_VERSION = '9.1.00';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
