269995fda9f56baa528a7f403de4e3cb6b41c476
fredtempez <ftempez@gmail.com>
2019-05-05 14:26:01 +0200

    [9.1.00] Hauteur de l'écran de connexion

---
+++ core/module/user/view/login/login.js.php
@@ -0,0 +1,18 @@
+/**
+ * This file is part of Zwii.
+ *
+ * For full copyright and license information, please see the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @authorFrédéric Tempez <frederic.tempez@outlook.com>
+ * @copyright Copyright (C) 2018-2019, Frédéric Tempez
+ * @license GNU General Public License, version 3
+ * @link http://zwiicms.com/
+ */
+
+/** 
+* Paramètres par défaut au chargement
+*/ 
+$( document ).ready(function() {
+   $("section").addClass("userLogin");
+});
