dc085e24ce6bfbbad6818ad925fa7300343a1848
fredtempez <ftempez@gmail.com>
2019-05-13 20:38:07 +0200

    [9.1.04] copyright

--- core/module/theme/view/footer/footer.js.php
+++ core/module/theme/view/footer/footer.js.php
@@ -7,8 +7,9 @@
  * @author Rémi Jean <remi.jean@outlook.com>
  * @copyright Copyright (C) 2008-2018, Rémi Jean
  * @license GNU General Public License, version 3
+ * @author Frédéric Tempez <frederic.tempez@outlook.com>
+ * @copyright Copyright (C) 2018-2019, Frédéric Tempez
  * @link http://zwiicms.com/
- * @Author 23/9/18 Frédéric Tempez <frederic.tempez@outlook.com>
  */
 
 /**
