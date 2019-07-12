dc7d5437acb77f4ef93bb26de2a7984344ae659a
fredtempez <ftempez@gmail.com>
2019-07-12 09:19:14 +0200

    [9.2.00] numéro de version

--- core/core.php
+++ core/core.php
@@ -32,7 +32,7 @@ class common {
 	const TEMP_DIR = 'site/tmp/';
 
 	// Numéro de version 
-	const ZWII_VERSION = '9.2.0v';
+	const ZWII_VERSION = '9.2.00';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
