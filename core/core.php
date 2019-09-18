8de6e2af0a1458feef4cc343fd3b9af8d762df9b
fredtempez <ftempez@gmail.com>
2019-09-18 18:31:40 +0200

    [9.2.06] autres petites corrections

--- core/core.php
+++ core/core.php
@@ -618,11 +618,7 @@ class common {
 						PHP_EOL .
 						'# ZWII CONFIG ---------' . PHP_EOL .
 						'User-agent: *' . PHP_EOL .
-						// 'Disallow: /core/' . PHP_EOL .
-						// 'Disallow: /module/' .PHP_EOL .
-						'Disallow: /site/data/' .PHP_EOL .
-						'Disallow: /site/tmp/' .PHP_EOL .
-						'Disallow: /site/backup/' .PHP_EOL .
+						'Disallow: /site/' .PHP_EOL .
 						'Allow: /site/file/' .PHP_EOL .
 						'Sitemap: ' . helper::baseUrl(false) . 'sitemap.xml' . PHP_EOL .
 						'Sitemap: ' . helper::baseUrl(false) . 'sitemap.xml.gz' . PHP_EOL .
@@ -904,7 +900,7 @@ class common {
 		// Version 9.0.09
 		if($this->getData(['core', 'dataVersion']) < 9009) {
 			$this->setData(['core', 'dataVersion', 9009]);
-			$this->SaveData();
+			$this->saveData();
 		}
 		// Version 9.0.10
 		if($this->getData(['core', 'dataVersion']) < 9010) {
