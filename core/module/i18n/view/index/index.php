0ef4ac1b08a2ff146a511fb069e84dc09ea6d67f
fredtempez <ftempez@gmail.com>
2019-10-22 08:42:11 +0200

    Nouvel écran de langues partie 1

--- core/module/i18n/view/index/index.php
+++ core/module/i18n/view/index/index.php
@@ -1,44 +1,25 @@
 <?php echo template::formOpen('i18nIndex'); ?>
 	<div class="row">
 		<div class="col2">
-			<?php echo template::button('configBack', [
+			<?php echo template::button('i18nBack', [
 				'class' => 'buttonGrey',
 				'href' => helper::baseUrl(false),
 				'ico' => 'home',
 				'value' => 'Accueil'
 			]); ?>
 		</div>
-		<div class="col2 offset8">
-			<?php echo template::submit('configSubmit'); ?>
+		<div class="col2 offset6">
+			<?php echo template::button('i18nAddButton', [
+				'href' => helper::baseUrl() . 'i18n/add',
+				'value' => 'Ajouter'
+			]); ?>
+		</div>	
+		<div class="col2">
+			<?php echo template::submit('i18nSubmit'); ?>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col8">
-			<div class="block">
-				<h4>Ajouter une localisation</h4>
-				<div class="row">			
-					<div class="col5">
-						<?php echo template::select('i18nLanguageCopyFrom', $this->i18nInstalled(true), [
-							'label' => 'Copier la structure de',
-							'help' => 'Ne rien sélectionner pour une copie vierge ',
-							'selected' => -1 					
-						]); ?>
-					</div>
-					<div class="col1">
-						<?php echo template::ico('right-big'); ?>
-					</div>
-					<div class="col5">
-						<?php 
-							$available = array ('' => 'Sélectionner');
-							$available = array_merge ($available, self::$i18nList);
-							echo template::select('i18nLanguageAdd', $available, [
-							'label' => 'vers'
-							]); ?>
-					</div>
-				</div>
-			</div>
-		</div>	
-		<div class="col4">
+	<div class="row">	
+		<div class="col12">
 			<div class="block">
 				<h4>Supprimer une localisation</h4>
 				<div class="row">
