dfb75a295258bc2f2f57bcd4df3cdb3427d14005
fredtempez <ftempez@gmail.com>
2019-11-01 19:30:53 +0100

    [10.0.62.dev] Aide dans la page de gestion des langues

--- core/module/i18n/view/index/index.php
+++ core/module/i18n/view/index/index.php
@@ -15,25 +15,31 @@
 	<div class="row">
 		<div class="col12">
 			<div class="block">
-				<h4>Ajouter une localisation</h4>
+				<h4>Ajouter une langue</h4>
 				<div class="row">			
-					<div class="col5">
+					<div class="col3 offset1">
 						<?php echo template::select('i18nLanguageCopyFrom', $this->i18nInstalled(true), [
-							'label' => 'Copier la structure de',
-							'help' => 'Ne rien sélectionner pour une copie vierge ',
+							'label' => 'Copier les pages de',
 							'selected' => -1 					
 						]); ?>
 					</div>
-					<div class="col1">
-						<?php echo template::ico('right-big'); ?>
+					<div class="col1 offset1 textAlignCenter">
+						<h3>▶</h3>
 					</div>
-					<div class="col5">
+					<div class="col3 offset1">
 						<?php 
 							$available = array ('' => 'Sélectionner');
 							$available = array_merge ($available, self::$i18nList);
 							echo template::select('i18nLanguageAdd', $available, [
-							'label' => 'vers'
+							'label' => 'Langue à créer'
 							]); ?>
+					</div>					
+				</div>
+				<div class="row">
+					<div class="col12">
+					<em>L'ajout d'une langue est concrétisé par la création d'un nouveau jeu de pages. Ce jeu de pages peut être minimal, dans ce cas
+					une seule page d'accueil est créée. Sinon vous pouvez sélectionner un jeu de page existant ce qui vous permet de réaliser
+					la traduction des textes sans avoir à créer de nouvelles pages.</em> 
 					</div>
 				</div>
 			</div>
