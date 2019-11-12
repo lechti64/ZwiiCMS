a1df57b24b64b79e841016cb5e3277a63533f1e0
Fred Tempez <ftempez@gmail.com>
2019-11-12 15:35:39 +0100

    [10.0.87.dev] commentaires dans init.php et langue courante dans main.php

--- site/i18n/init.php
+++ site/i18n/init.php
@@ -2,26 +2,28 @@
 
 
 /*
-* Pour ajouter une langue
+* Pour ajouter une langue et son drapeau
+* Il s'agit bien d'un drapeau relatif à une langue etnon à un pays, on ne doit donc pas insérer de pays comme la Belgique ou la Suisse.
+*
 * Soit la langue est disponible dans la liste prédéfinie, il suffit de supprimer les deux // de commentaire
 * Soit la langue n'est pas disponible, complétez le tableau en ajoutant 
 *  1 - Le code iso sur deux caractères en vous  référant à cette page https://fr.wikipedia.org/wiki/ISO_3166-1 
 *  2 - Le nom de la langue en texte en clair, le rappel du code iso est facultatif
 *  3 - Enfin, créez  dans le dossier png le drapeau de la langue au format png dimensions 30x20 pixels
 *
-* Attention : Il s'agit bien d'un drapeau relatif à une langue etnon à un pays, on ne doit donc pas insérer de pays comme la Belgique ou la Suisse.
+* Le dernier élément de la liste n'a pas besoin de ,
 */
 
 $i18nOptions = [
 	//'bg'	=> 'Bulgare (bg)',
-	'dk'	=> 'Danois (dk)',
+	//'dk'	=> 'Danois (dk)',
 	//'fi'	=> 'Finnois (fi)',		
 	//'is' 	=> 'Islandais (is)',		
-	'no'	=> 'Norvégien (no)',
+	//'no'	=> 'Norvégien (no)',
 	//'se'	=> 'Suédois (se)',
 	//'ro'	=> 'Roumain (ro)',
-	'cz'	=> 'Tchèque (cz)',
-	//'tr'	=> 'Turc (tr)',
+	//'cz'	=> 'Tchèque (cz)'
+	//'tr'	=> 'Turc (tr)'
 	// Langues régionales
 	//'eu'	=> 'Basque (eu)',
 	//'br'	=> 'Breton (br)',
