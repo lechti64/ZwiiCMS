f1486adb132a2ec4facb5b05d59ed843185e34d7
fredtempez <ftempez@gmail.com>
2019-11-19 19:24:49 +0100

    [10.0.119.dev] traduction des légendes affichées

--- core/core.php
+++ core/core.php
@@ -32,7 +32,7 @@ class common {
 	const I18N_DIR = 'site/i18n/';
 
 	// Numéro de version 
-	const ZWII_VERSION = '10.0.118.dev';
+	const ZWII_VERSION = '10.0.119.dev';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
--- core/vendor/simplelightbox/init.js
+++ core/vendor/simplelightbox/init.js
@@ -1,10 +1,10 @@
 	// Paramètres d'initialisation
 	$(document).ready(function() {
-			// Ajouter la classe Gallery afin de faire la liaison avec simplelightbox
+		// Ajouter la classe Gallery afin de faire la liaison avec simplelightbox
+		$("a[rel='gallery']").addClass("gallery translate");
 
-
-		$("a[rel='gallery']").addClass(
-			"gallery",""
-		);
 		$('.gallery').simpleLightbox({closeText:"&times;",captionsData:'alt'});
+		
+		// Traduite les légendes
+		$('body').removeClass("notranslate");		
 	});
