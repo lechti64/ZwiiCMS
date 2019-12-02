dcde62917b3deabbd620d8f8f5e13ca264c80c40
Fred Tempez <ftempez@gmail.com>
2019-12-02 12:48:17 +0100

    [9.2.13] Restaurer le module redirection dans la version 1.3 et le core

--- core/core.php
+++ core/core.php
@@ -1393,15 +1393,9 @@ class core extends common {
 							}
 							// Redirection
 							if($output['redirect']) {
-								if (!empty($output['script']) ) {
-									$this->addOutput ([ 										
-										'script' => $output['script']
-									]);
-								} else {
-									http_response_code(301);
-									header('Location:' . $output['redirect']);
-									exit();
-								}
+								http_response_code(301);
+								header('Location:' . $output['redirect']);
+								exit();								
 							}
 						}
 						// Données en sortie applicables même lorsqu'une notice est présente
