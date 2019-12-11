ab26ff2cec021b11e1b242e2eacf2ca01062cd14
fredtempez <ftempez@gmail.com>
2019-12-11 22:42:35 +0100

    (10.0.010.dev] version

--- core/core.php
+++ core/core.php
@@ -34,7 +34,7 @@ class common {
 	const TEMP_DIR = 'site/tmp/';
 
 	// Numéro de version 
-	const ZWII_VERSION = '10.0.009.dev';
+	const ZWII_VERSION = '10.0.010.dev';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
