4c95df54ab5aa57e936926905753fa721d4170f6
fredtempez <ftempez@gmail.com>
2019-12-11 16:16:43 +0100

    [9.2.15] Couleur du titre du site dans le menu réduit

--- core/core.php
+++ core/core.php
@@ -1172,7 +1172,8 @@ class core extends common {
 				$css .= 'nav a.active{background-color:' . $this->getData(['theme','menu','activeColor']) . '}';
 				$color2 = helper::colorVariants($this->getData(['theme', 'menu', 'textColor']));
 				$css .= 'nav a.active{color:' .  $color2['text'] . '}';
-			}				
+			}		
+			$css .= 'nav #burgerText{color:' .  $colors['text'] . '}';		
 			$css .= 'nav .navLevel1 a.active {border-radius:' . $this->getData(['theme', 'menu', 'radius']) . '}'; 
 			$css .= '#menu{text-align:' . $this->getData(['theme', 'menu', 'textAlign']) . '}';
 			if($this->getData(['theme', 'menu', 'margin'])) {
