5c752c993ebbec8e2a826161348e9ea718e519a0
Fred Tempez <ftempez@gmail.com>
2020-01-21 08:42:21 +0100

    [10.0.025.dev] Erreurs de déclaration

--- core/core.php
+++ core/core.php
@@ -34,7 +34,7 @@ class common {
 	const TEMP_DIR = 'site/tmp/';
 
 	// Numéro de version 
-	const ZWII_VERSION = '10.0.024.dev';
+	const ZWII_VERSION = '10.0.025.dev';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
