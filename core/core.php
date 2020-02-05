e6ce520ea09a5d9c51b6d48e4334f65040551ad2
Fred Tempez <ftempez@gmail.com>
2020-02-05 16:47:43 +0100

    [9.2.18] Mentions légales dans le site de test

--- core/core.php
+++ core/core.php
@@ -941,8 +941,8 @@ class common {
 		// Version 9.2.00
 		if($this->getData(['core', 'dataVersion']) < 9200) {
 			$this->setData(['theme','footer','template', 3 ]);
-			$this->setData(['theme','footer','margin', true ]);
-			$this->setData(['theme','footer','displayLegal', false ]);
+			$this->setData(['theme','footer','margin', true ]);			
+			$this->setData(['theme','footer','displayLegal', !empty($this->getdata(['config','legalPageId'])) ]);
 			$this->setData(['theme','footer','displaySearch', false ]);
 			$this->setData(['config','social','githubId', '' ]);
 			$this->setData(['core', 'dataVersion', 9200]);
@@ -1047,7 +1047,7 @@ class common {
 			$this->setData(['core', 'baseUrl', helper::baseUrl(true,false) ]);
 			$this->setData(['core', 'dataVersion', 9216]);
 			$this->saveData();
-		}		
+		}
 	}
 }
 
