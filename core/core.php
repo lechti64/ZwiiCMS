8c7efe9aa816480b7ea28cd1b270c4065c917e9e
fredtempez <ftempez@gmail.com>
2020-03-01 16:49:07 +0100

    Merge master 9.2.23

--- core/core.php
+++ core/core.php
@@ -2248,7 +2248,6 @@ class layout extends common {
 				if( $this->getData(['config','autoUpdate']) === true &&
 					$lastAutoUpdate > $this->getData(['core','lastAutoUpdate']) + 604800 ) {	
 						$this->setData(['core','lastAutoUpdate',$lastAutoUpdate]);
-						$this->saveData();
 				    if ( helper::checkNewVersion(common::ZWII_UPDATE_CHANNEL)  ) {
 						$rightItems .= '<li><a id="barUpdate" href="' . helper::baseUrl() . 'install/update" data-tippy-content="Mettre à jour Zwii '. common::ZWII_VERSION .' vers '. helper::getOnlineVersion(common::ZWII_UPDATE_CHANNEL) .'">' . template::ico('update colorRed') . '</a></li>';
 					}
