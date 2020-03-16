01b5f3458a2f6b512dae31d8179a243377d999fe
Fred Tempez <ftempez@gmail.com>
2020-03-16 15:27:53 +0100

    [9.2.25] Thème menu monter la boite fonte pour faciliter la prévisualition

--- core/module/theme/view/menu/menu.php
+++ core/module/theme/view/menu/menu.php
@@ -57,26 +57,6 @@
 <div class="row">
     <div class="col12">
         <div class="block">
-        <h4>Contenus</h4>
-            <div class="row">
-                <div class="col6">
-                        <?php echo template::checkbox('themeMenuLoginLink', true, 'Lien de connexion', [
-                                'checked' => $this->getData(['theme', 'menu', 'loginLink'])
-                            ]); ?>
-                </div>
-                <div class="col6">
-                        <?php echo template::checkbox('themeMenuBurgerTitle', true, 'Titre du site dans le menu réduit', [
-                                'checked' => $this->getData(['theme', 'menu', 'burgerTitle']),
-                                'help' => 'Le menu burger remplace le menu complet lorsque la largeur de l\'écran  n\'est pas suffisante.'
-                            ]); ?>
-                </div>
-            </div>                
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col12">
-        <div class="block">
             <h4>Mise en forme du texte</h4>
             <div class="row">
                 <div class="col6">
@@ -165,6 +145,26 @@
 
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col12">
+        <div class="block">
+        <h4>Contenus</h4>
+            <div class="row">
+                <div class="col6">
+                        <?php echo template::checkbox('themeMenuLoginLink', true, 'Lien de connexion', [
+                                'checked' => $this->getData(['theme', 'menu', 'loginLink'])
+                            ]); ?>
+                </div>
+                <div class="col6">
+                        <?php echo template::checkbox('themeMenuBurgerTitle', true, 'Titre du site dans le menu réduit', [
+                                'checked' => $this->getData(['theme', 'menu', 'burgerTitle']),
+                                'help' => 'Le menu burger remplace le menu complet lorsque la largeur de l\'écran  n\'est pas suffisante.'
+                            ]); ?>
+                </div>
+            </div>                
         </div>
     </div>
 </div>
