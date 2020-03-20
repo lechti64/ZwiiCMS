23aa89069ef51dd46ab5df77bd61973fbd0761bb
Fred Tempez <ftempez@gmail.com>
2020-03-20 15:08:10 +0100

    [9.2.26] suppression ligne en trop

--- core/vendor/tinymce/init.js
+++ core/vendor/tinymce/init.js
@@ -191,13 +191,6 @@ tinymce.init({
 	]
 });
 
-/**
-  * Suppression du back ground TinyMCE
-  * 
-  */
- $(body).removeAttr("background-image");
-
-
 tinymce.PluginManager.add('stickytoolbar', function(editor, url) {
 	editor.on('init', function() {
 	  setSticky();
