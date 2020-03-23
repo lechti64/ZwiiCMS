6cad85c285b431083433e41eaf3132f05b8e137b
Fred Tempez <ftempez@gmail.com>
2020-03-23 16:27:32 +0100

    Version

--- core/core.php
+++ core/core.php
@@ -33,7 +33,7 @@ class common {
 	const TEMP_DIR = 'site/tmp/';
 
 	// Numéro de version 
-	const ZWII_VERSION = '10.0.044';
+	const ZWII_VERSION = '10.0.045';
 	const ZWII_UPDATE_CHANNEL = "v10";
 
 	public static $actions = [];
