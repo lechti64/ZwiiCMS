f4902bfed7527dd9909b36ed018d15e0a9785d9d
Fred Tempez <ftempez@gmail.com>
2020-03-28 19:31:22 +0100

    [9.3.00] nettoyage

--- module/gallery/gallery.php
+++ module/gallery/gallery.php
@@ -44,8 +44,6 @@ class gallery extends common {
 	public function config() {
 		// Liste des galeries
 		$galleries = $this->getData(['module', $this->getUrl(0)]);
-		$countGalleries = count($this->getData(['module',$this->getUrl(0)]));
-
 		if($galleries) {	
 			foreach($galleries as $galleryId => $gallery) {
 				// Erreur dossier vide
